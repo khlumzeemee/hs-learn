0dcfc3071567e19091f8ef0f7afa17ec4fcb881f
Allen Sooredoo <asooredoo@gmaile.com>
2019-10-16 10:15:06 +0200

    chap 8

---
+++ src/LearnYouHaskell/Chap8_Own_types/Person.hs
@@ -0,0 +1,24 @@
+module LearnYouHaskell.Chap8_Own_types.Person where
+
+data Person = Person {
+    firstName :: String
+    , lastName :: String
+    , age :: Int
+    -- , height :: Float
+    -- , phoneNumber ::  String
+    -- , flavor :: String
+    } deriving (Eq, Show, Read)
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
